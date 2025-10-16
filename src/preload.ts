b9f6dd21a4e19e59f5940dfc3aa0c790f403cedc
abrahamVado <abraham.vado@gmail.com>
2025-10-15 20:38:49 -0600

    chore init

--- src/preload.ts
+++
@@ -1,4 +0,0 @@
-import { contextBridge, ipcRenderer } from 'electron';
-const NS = 'ichiba' as const;
-const api = { ping: () => ipcRenderer.invoke(`${NS}:ping`) };
-contextBridge.exposeInMainWorld(NS, api);
